98bb37f06a37419f931203422cbcac7dae27839e
howooking <junsgk@gmail.com>
2023-04-20 08:22:11 +0900

    settings : 폰트 설정, favicon 추가
    - Oswald, Roboto link 방식으로 폰트 설정
    - backup 폰트 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import Navbar from './components/Navbar';
+
 export default function App(): JSX.Element {
-  return <div className="text-red-300">hello world</div>;
+  return (
+    <div className="font-body">
+      <Navbar />
+      <h1 className="font-Oswald">the Quick fox jumps over the lazy dod</h1>
+      <h1 className="font-bold">the Quick fox jumps over the lazy dod</h1>
+      <h1>the Quick fox jumps over the lazy dod</h1>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      Oswald: ['Oswald'],
+      body: [
+        'Roboto',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
     extend: {},
   },
   plugins: [],
