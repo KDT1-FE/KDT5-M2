3fa999a5c2a7b0b4b8bad57b33586312aff8bcae
pildrums <there7788@gmail.com>
2023-04-19 22:34:15 +0900

    Delete theme

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./lib/styles/GlobalStyle";
-import theme from "./lib/styles/colors";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
