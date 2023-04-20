fb179ebf154c325f5fbd54ceb6e696369e96e7c1
pildrums <there7788@gmail.com>
2023-04-20 15:08:22 +0900

    Add Space component > header가 position: fixed > 다음 컨텐츠 영역을 덮을 우려가 있어서 styled-components로 Space라는 컴포넌트 생성 > margin-bottom의 속성을 header 높이만큼 지정

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import styled from "styled-components";
 import { getMovie } from "./lib/api/movieApi";
 import Header from "/src/components/Header";
 
@@ -5,8 +7,13 @@ function App() {
   return (
     <>
       <Header />
+      <Space />
     </>
   );
 }
+
+const Space = styled.div`
+  margin-bottom: 60px;
+`;
 
 export default App;
