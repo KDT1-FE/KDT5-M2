6be55d60772ba11b22b84ea274ea8d1a0a2ac018
pildrums <there7788@gmail.com>
2023-04-21 23:41:07 +0900

    필요없거나 잘못 만들어진 파일 제거

--- src/lib/api/movieApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function getMovies() {
-  const { data } = await axios({
-    url: `https://omdbapi.com/?apikey=7035c60c&s=frozen`,
-    method: "GET",
-  });
-  console.log(data.Search);
-}
