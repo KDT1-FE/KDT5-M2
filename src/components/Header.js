9cfd6dff7a73d9027e9a3f0bfea3262f1f7f9bf4
pildrums <there7788@gmail.com>
2023-04-22 00:54:09 +0900

    검색창 UI 및 애니메이션 구현 완료 > 클릭 시, 버튼 색상 변환 (활성화: red, 비활성화: black) > 검색창과 선택 컴포넌트를 오른쪽에서 왼쪽으로 펼쳐지는 애니메이션 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,37 @@
 import styled from "styled-components";
 import { MdSearch } from "react-icons/md";
 import { useRef, useState } from "react";
+import { colors } from "../styles/colors";
 
 function Header() {
   const [searchOpen, setSearchOpen] = useState(false);
+  const [color, setColor] = useState();
   const scrollRef = useRef();
+
   const handleSearchOpen = () => {
     setSearchOpen(!searchOpen);
+    setColor(!color);
   };
+
   return (
     <Container>
       <HeaderContent>
         <h1>MovieApp</h1>
-        <Search>
-          <SearchButton onClick={handleSearchOpen} />
-          <form className="active">
+        <SearchContainer>
+          <SearchButton
+            onClick={handleSearchOpen}
+            className={color && "btn-active"}
+          />
+          <Search className={searchOpen && "active"}>
             <select>
               <option value="">1</option>
               <option value="">1</option>
               <option value="">1</option>
               <option value="">1</option>
             </select>
-            <input type="text" />
-          </form>
-        </Search>
+            <input type="text" placeholder="영화를 입력하세요" />
+          </Search>
+        </SearchContainer>
       </HeaderContent>
     </Container>
   );
@@ -31,18 +39,19 @@ function Header() {
 
 const Container = styled.header`
   height: 60px;
-  background: coral;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 0 20px;
+  border-bottom: 1px solid ${colors.gray[7]};
+  background: ${colors.gray[0]};
 `;
 
 const HeaderContent = styled.div`
   max-width: 1280px;
   width: 1280px;
   height: inherit;
-  background: blueviolet;
+  /* background: blueviolet; */
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -52,22 +61,49 @@ const HeaderContent = styled.div`
   }
 `;
 
-const SearchButton = styled(MdSearch)`
-  font-size: 36px;
-  cursor: pointer;
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  /* background: coral; */
 `;
 
-const Search = styled.div`
+const Search = styled.form`
   display: flex;
   justify-content: center;
   align-items: center;
-  form {
-    opacity: 0;
-    width: 0;
-    .active {
-      opacity: 1;
-      width: auto;
-    }
+  transition: all 0.3s ease-in-out;
+  width: 0;
+  visibility: hidden;
+  opacity: 0;
+  select {
+    width: 100px;
+    height: 40px;
+    border: none;
+    margin-right: 10px;
+    background: transparent;
+    border-bottom: 2px solid ${colors.gray[9]};
+  }
+  input {
+    width: 180px;
+    height: 40px;
+    border: none;
+    background: transparent;
+    border-bottom: 2px solid ${colors.gray[9]};
+  }
+  &.active {
+    width: 290px;
+    visibility: visible;
+    opacity: 1;
+  }
+`;
+
+const SearchButton = styled(MdSearch)`
+  font-size: 36px;
+  cursor: pointer;
+  color: ${colors.gray[9]};
+  &.btn-active {
+    color: ${colors.red[6]};
   }
 `;
 
