f595329c3dc855387c923ffcf5358052fb238ac4
pildrums <there7788@gmail.com>
2023-04-21 22:10:35 +0900

    메인 페이지 라우트 컴포넌트 작성

---
+++ src/routes/MainPage.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+
+function MainPage() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default MainPage;
