f5ecd3742bb1a4054a89738357a7f6aea00b2e49
pildrums <there7788@gmail.com>
2023-04-22 01:51:39 +0900

    OMDB API 함수 적용 > 현재 MainPage에 movies라는 이름의 초기상태 props 전달 > getMovies 함수는 try-catch 구문으로 예외처리 > useEffect로 state 변동 후 getMovies 함수 호출

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ErrorPage from "./routes/ErrorPage";
 import MainPage from "./routes/MainPage";
 import SearchPage from "./routes/SearchPage";
 
 function App() {
+  // Hooks
+  const [movies, setMovies] = useState([]);
+
+  // Function
+  /**
+    @todo getMovies 함수 모듈화 필요
+   */
+  const getMovies = async () => {
+    try {
+      const url = `http://omdbapi.com/?apikey=7035c60c&s=frozen`;
+      const res = await fetch(url);
+      const json = await res.json();
+      if (json.Search) {
+        setMovies(json.Search);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<MainPage movies={movies} />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
