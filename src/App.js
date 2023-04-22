ded8ad72ddc5634a8784839892557d5a4fa9ca88
pildrums <there7788@gmail.com>
2023-04-23 01:17:46 +0900

    Header 메뉴 관련 state와 함수 작성 > useState를 이용해서 category 초기 상태값 정의 > useCallback을 이용해서 카테고리 값 업데이트 > Header 컴포넌트에 props로 category, onMenuSelect 전달

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import ErrorPage from "./routes/ErrorPage";
 import MainPage from "./routes/MainPage";
+import MoviePage from "./routes/MoviePage";
 import SearchPage from "./routes/SearchPage";
 
 function App() {
   // Hooks
   const [movies, setMovies] = useState([]);
-
+  const [category, setCategory] = useState("검색");
   // Function
   /**
     @todo getMovies 함수 모듈화 필요
@@ -25,15 +27,19 @@ function App() {
     }
   };
 
+  const onMenuSelect = useCallback((category) => setCategory(category), []);
+
   useEffect(() => {
     getMovies();
   }, []);
 
   return (
     <Router>
+      <Header category={category} onMenuSelect={onMenuSelect} />
       <Routes>
         <Route path="/" element={<MainPage movies={movies} />} />
         <Route path="/search" element={<SearchPage />} />
+        <Route path="/movies" element={<MoviePage />} />
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </Router>
