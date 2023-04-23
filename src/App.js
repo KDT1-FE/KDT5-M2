fbf3023851ac58f23572ae0b3dfcbb4156cd02c6
pildrums <there7788@gmail.com>
2023-04-24 03:39:12 +0900

    process.env.PUBLIC 추가 및 SearchPage 라우팅 적용

--- src/App.js
+++ src/App.js
@@ -8,37 +8,16 @@ import SearchPage from "./routes/SearchPage";
 
 function App() {
   // Hooks
-  const [movies, setMovies] = useState([]);
   const [category, setCategory] = useState("검색");
-  // Function
-  /**
-    @todo getMovies 함수 모듈화 필요
-   */
-  const getMovies = async () => {
-    try {
-      const url = `http://omdbapi.com/?apikey=7035c60c&s=frozen`;
-      const res = await fetch(url);
-      const json = await res.json();
-      if (json.Search) {
-        setMovies(json.Search);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
   const onMenuSelect = useCallback((category) => setCategory(category), []);
 
-  useEffect(() => {
-    getMovies();
-  }, []);
-
+  // Render
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header category={category} onMenuSelect={onMenuSelect} />
       <Routes>
-        <Route path="/" element={<MainPage movies={movies} />} />
-        <Route path="/search" element={<SearchPage />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/search/:title" element={<SearchPage />} />
         <Route path="/movies" element={<MoviePage />} />
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
