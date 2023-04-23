0c755e29d50877681bf216e137106d529de3dda3
pildrums <there7788@gmail.com>
2023-04-24 04:57:41 +0900

    검색결과 결과 없을 때 메시지 컴포넌트 작성

---
+++ src/components/SearchEmpty.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { colors } from "../lib/styles/colors";
+
+function SearchEmpty() {
+  return (
+    <EmptyContainer>
+      <Empty>
+        <span>검색결과가 없습니다.</span>
+      </Empty>
+    </EmptyContainer>
+  );
+}
+
+const EmptyContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 0 20px;
+`;
+
+const Empty = styled.div`
+  background: ${colors.gray[0]};
+  width: 1280px;
+  max-width: 1280px;
+  padding: 30px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default SearchEmpty;
