f0e7c7ef56b68bae153f076728274f8fa0bfedce
pildrums <there7788@gmail.com>
2023-04-24 03:40:41 +0900

    useEffect 삭제

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import ErrorPage from "./routes/ErrorPage";
