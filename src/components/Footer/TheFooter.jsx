0b5164d1e8e05b6f236901c983189194f133867e
Kled <1017yu@daum.net>
2023-04-24 14:16:53 +0900

    feat: <Footer /> 추가

---
+++ src/components/Footer/TheFooter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./TheFooter.module.scss";
+
+const TheFooter = () => {
+  return (
+    <>
+      <footer>
+        <div className={styles.footerDiv}>
+          <h3>hi</h3>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default TheFooter;
