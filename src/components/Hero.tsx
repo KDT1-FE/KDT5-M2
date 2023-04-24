a39f94df8de0a8031b1b177d357c4336f413a4b4
howooking <junsgk@gmail.com>
2023-04-24 10:14:35 +0900

    feat : 홈화면 hero 섹션 컴포넌트

---
+++ src/components/Hero.tsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+  return (
+    <>
+      <p className="font-Oswald text-7xl mt-10">
+        <span className="text-amber-400">OMDb API</span>
+        <br />
+        THE OPEN
+        <br />
+        MOVIE DATABASE
+      </p>
+      <p className="my-7 text-gray-700">
+        The OMDb API is a RESTful web service to obtain movie information, all
+        content and images on the site are contributed and maintained by our
+        users.
+        <br />
+        If you find this service useful, please consider making a one-time
+        donation or become a patron.
+      </p>
+    </>
+  );
+}
