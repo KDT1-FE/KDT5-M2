e763091b24d7e0b1b918cec41299b600fcfa1201
Kled <1017yu@daum.net>
2023-04-24 20:48:18 +0900

    feat: 무한 스크롤 구현 작업 중

--- src/core/movieData.js
+++ src/core/movieData.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-export async function fetchMovies(inputText) {
+export async function fetchMovies(inputText, page) {
   const { data } = await axios({
-    url: `https://omdbapi.com/?apikey=7035c60c&s=${inputText}`,
+    url: `https://omdbapi.com/?apikey=7035c60c&s=${inputText}&page=${page}`,
     method: "GET",
   });
-  // ` || [] `:  array.map 오류 방지
-  console.log(data);
-  return data.Search || [];
+
+  return data;
 }
