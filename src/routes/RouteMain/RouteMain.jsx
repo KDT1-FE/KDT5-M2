ca0192838bf598f568f56310d7cd0ac8588c7808
Kled <1017yu@daum.net>
2023-04-24 22:09:09 +0900

    docs: test

--- src/routes/RouteMain/RouteMain.jsx
+++ src/routes/RouteMain/RouteMain.jsx
@@ -6,32 +6,31 @@ import styles from "./RouteMain.module.scss";
 // path를 통해 movieId를 수신
 const RouteMovie = () => {
   const [movie, setMovie] = useState({});
-  const params = useParams();
+  const { movieId } = useParams();
 
   // useEffect 내에서 async await 함수를 만들고 그 함수(fetchDetailMovie)를 바로 호출
   // useEffect는 항상 함수를 반환해야하는데, 그렇지 않으면 Promise를 반환하게 되어 error 발생
   useEffect(() => {
-    async function fetchDetailMovie() {
+    async function fetchDetailMovie(movieId) {
       const res = await fetch(
-        `https://omdbapi.com/?apikey=7035c60c&i=${params.movieId}`
+        `https://omdbapi.com/?apikey=7035c60c&i=${movieId}`
       );
       const result = await res.json();
       setMovie(result);
-      console.log(result);
     }
-    fetchDetailMovie();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    fetchDetailMovie(movieId);
+  }, [movieId]);
+
+  // React.StrictMode를 사용하지 않음에도 useEffect의 영향으로 두번 렌더링하는 현상 발생 -> replace로 리사이징 불가.
+  // const bigPoster = movie.Poster.replace("SX300", "SX500")
+  console.log(movie.Poster);
 
   return (
     <main>
       <article className={styles.mainArticle}>
         <div className={styles.movieContainer}>
-          <div
-            className={styles.moviePoster}
-            style={{ background: `url(${movie.Poster}) alt=${movie.Title}` }}
-          >
-            {" "}
+          <div className={styles.moviePoster}>
+            <img className={styles.moviePosterImg} src={movie.Poster} alt="" />
           </div>
           <div className={styles.movieInfo}>
             <h2>{movie.Title}</h2>
