9f0590eaae1582776cbfed247d7b0367e2d2b6ba
Kled <1017yu@daum.net>
2023-04-24 20:47:50 +0900

    feat: Router 관리 작업

--- src/routes/Movie.jsx
+++ src/routes/Movie.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TheHeader from "../components/Header/TheHeader";
+import RouteMain from "./RouteMain/RouteMain";
 
 export default function Movie() {
   return (
     <>
       <TheHeader />
+      <RouteMain />
     </>
   );
 }
