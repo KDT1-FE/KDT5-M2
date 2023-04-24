caa759a993f9c9d83b1d59cb5cc5f6e577b94465
Kled <1017yu@daum.net>
2023-04-24 20:46:53 +0900

    feat: 비동기 처리로 setMessage 구현

--- src/components/Main/SearchInput/SearchInput.jsx
+++ src/components/Main/SearchInput/SearchInput.jsx
@@ -6,28 +6,38 @@ import SearchIcon from "@mui/icons-material/Search";
 const SearchInput = () => {
   const [inputText, setInputText] = useState("");
   const [movies, setMovies] = useState([]);
+  const [message, setMessage] = useState("검색을 해보세요!");
+  // const [page, setPage] = useState("1");
 
-  // TitleSearchHandler: input의 value로 들어오는 값을 setInputText로 동적으로 다룬다.
+  // TitleSearchHandler: input의 value로 들어오는 값을 setInputText로 동적으로 다룬다.movies
   const TitleSearchHandler = (event) => {
     setInputText(event.target.value);
   };
 
-  // 비동기 처리 함수 pressEnterKey: Enter keydown시 inputText의 값을 fetchMovies의 input값으로 처리, 결과 값(movieList)을 setMovies로 동적으로 다룬다.
+  // 비동기 처리 함수 pressEnterKey: Enter keydown시 inputText의 값을 fetchMovies의 input값으로 처리, 결과 값(movieData.Search)을 setMovies로 동적으로 다룬다.
   async function pressEnterKey(event) {
     if (event.key === "Enter") {
       // Search Movie
       if (!inputText.trim()) return;
-      const movieList = await fetchMovies(inputText);
-      setMovies(movieList);
+      const movieData = await fetchMovies(inputText);
+      // ` || [] `:  array.map 오류 방지
+      setMovies(movieData.Search || []);
+      // 검색 결과가 Truthy면 message를 빈 문자열화, Falsy(= 검색결과 없음)면 검색 결과가 없다는 문자열 출력!
+      movieData.Search ? setMessage("") : setMessage("검색 결과가 없습니다.");
     }
   }
 
-  // SearchButton 클릭 시 동일하게 async function으로 movieList 출력
+  // SearchButton 클릭 시 동일하게 async function으로 movieList 출력 && 추후에 pressEnterKey 함수와 병합 작업 예정
   async function pressSearchButton() {
     if (!inputText.trim()) return;
-    const movieList = await fetchMovies(inputText);
-    setMovies(movieList);
+    const movieData = await fetchMovies(inputText).Search;
+    setMovies(movieData.Search || []);
+    movieData.Search ? setMessage("") : setMessage("검색 결과가 없습니다.");
   }
+
+  // useEffect(() => {
+  //   movies.length ?
+  // });
 
   return (
     <>
@@ -47,21 +57,21 @@ const SearchInput = () => {
         </div>
 
         <div className={styles.searchedMovies}>
-          <div className={styles.moviesWrapper}>
-            <div className={styles.message}>!</div>
+          <ul className={styles.moviesWrapper}>
+            {message}
             {movies.map((movie) => (
-              <div className={styles.movies}>
+              <li key={movie.imdbID} className={styles.movies}>
                 <a
                   className={styles.movie}
                   href={`/movie/${movie.imdbID}`}
                   style={{ background: `url(${movie.Poster})` }}
                 >
-                  ''
+                  {" "}
                 </a>
                 <p className={styles.movieInfo}>{movie.Title}</p>
-              </div>
+              </li>
             ))}
-          </div>
+          </ul>
         </div>
       </section>
     </>
