4f23f3249f981f40ee810f3846baa7b840544e82
pildrums <there7788@gmail.com>
2023-04-24 21:24:36 +0900

    Main 컴포넌트 작성 > 페이지 제목 / 소제목 / 설명 작성 > UI 작업 완료

---
+++ src/components/Main.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import { colors } from "../lib/styles/colors";
+
+function Main() {
+  return (
+    <MainContainer>
+      <MainItem>
+        <MainTitle>
+          <span>MovieApp</span>
+          <h3>THE OPEN</h3>
+          <h3>MOVIE DATABASE</h3>
+        </MainTitle>
+        <Description>
+          The OMDb API is a RESTful web service to obtain movie information, all
+          content and images on <br /> the site are contributed and maintained by our
+          users. <br /> If you find this service useful, please consider making
+          a one-time donation or become a patron.
+        </Description>
+      </MainItem>
+    </MainContainer>
+  );
+}
+
+const MainContainer = styled.div`
+  width: 100%;
+  padding: 0 20px;
+  margin-top: 120px;
+  display: flex;
+  justify-content: center;
+`;
+
+const MainItem = styled.div`
+  width: 1280px;
+  max-width: 1280px;
+`;
+
+const MainTitle = styled.div`
+  span {
+    margin: 20px 0 0;
+    font-size: 80px;
+    font-weight: 300;
+    color: ${colors.lime[4]};
+  }
+  h3 {
+    font-size: 80px;
+    font-weight: 300;
+    color: ${colors.gray[2]};
+    letter-spacing: -2px;
+    &:last-child {
+      line-height: 30px;
+    }
+  }
+`;
+
+const Description = styled.p`
+  margin-top: 60px;
+  color: ${colors.gray[5]};
+  font-size: 1rem;
+  word-spacing: -2px;
+`;
+
+export default Main;
