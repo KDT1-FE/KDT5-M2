f47de4a5573a34b91898a3b768eeef539aefe53d
pildrums <there7788@gmail.com>
2023-04-25 01:18:35 +0900

    기존에 api 불러오는 함수 api/movieAPI.js로 이동

--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -1,52 +1,12 @@
-import { useEffect, useState } from "react";
-import Loading from "../components/Loading";
-import Main from "../components/Main";
-import MovieList from "../components/MovieList";
-import SearchEmpty from "../components/SearchEmpty";
-import SearchForm from "../components/SearchForm";
+import Search from "../components/search/Search";
+import Main from "../components/search/SearchMain";
+
 
 function MainPage() {
-  const [searchValue, setSearchValue] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const getMovies = async (searchValue) => {
-      setLoading(true);
-      try {
-        const url = `http://omdbapi.com/?apikey=7035c60c&s=${searchValue}`;
-        const res = await fetch(url);
-        const resJson = await res.json();
-        if (resJson.Search) {
-          setMovies(resJson.Search);
-        }
-      } catch (e) {
-        console.error(e);
-      }
-      setTimeout(() => setLoading(false), 2000);
-      clearTimeout(setLoading(false));
-    };
-    getMovies(searchValue);
-  }, [searchValue]);
-
   return (
     <>
       <Main />
-      <SearchForm
-        movies={movies}
-        searchValue={searchValue}
-        setMovies={setMovies}
-        setSearchValue={setSearchValue}
-      />
-      {loading ? (
-        <Loading />
-      ) : movies.length === 0 ? (
-        <SearchEmpty />
-      ) : searchValue === "" ? (
-        <SearchEmpty />
-      ) : (
-        <MovieList movies={movies} />
-      )}
+      <Search />
     </>
   );
 }
