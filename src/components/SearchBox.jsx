5847edc00eab70b549780385054c1e3a29cb9a40
hookor <gerrard1219@gmail.com>
2023-04-25 22:38:46 +0900

    SearchBar

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,5 +1,5 @@
 import { React } from 'react'
-import { InputBase, Box, Stack } from '@mui/material'
+import { InputBase, Box, Stack, InputAdornment } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
 
 const SearchBox = props => {
@@ -15,10 +15,15 @@ const SearchBox = props => {
           bgcolor: 'tertiary.main',
           borderRadius: '10px',
           pl: '10px',
-          mr: '30px'
-          // '&:hover': {}
+          mr: '30px',
+          '&:hover': { bgcolor: '#e7ebf0', transition: '.2s linear' }
         }}>
         <InputBase
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          }
           placeholder="Search"
           value={props.value}
           onKeyDown={event => {
