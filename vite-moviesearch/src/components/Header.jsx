145e17b19f44ec964b2785c1f35de0a77c323567
leechanghwi <lih120@naver.com>
2023-04-26 22:15:45 +0900

    수정본

--- vite-moviesearch/src/components/Header.jsx
+++ vite-moviesearch/src/components/Header.jsx
@@ -1,56 +1,54 @@
-import favicon from '../assets/favicon.ico'
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import favicon from "../assets/favicon.ico";
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
     <header className="display: flex h-20 relative flex items-center px-10 ">
-      <a
-        className="text-xl font-extrabold  w-28 h-6 mr-20"
-        href="/">
+      <a className="text-xl font-extrabold  w-28 h-6 mr-20" href="/">
         <span className="text-regal-yellow ">OMDbAPI</span>
         .COM
       </a>
       <nav>
         <ul className="display: flex flex-wrap: wrap text-xl text-regal-yellow ">
           <li
-            className={`w-20 m-0 h-9 flex items-center justify-center ${
-              location.pathname === '/'
-                ? 'bg-regal-yellow text-white'
-                : 'active:bg-regal-yellow hover:text-yellow-300 '
-            } rounded-lg py-2 px-4`}>
+            className={`w-20 m-0 h-10 flex items-center justify-center ${
+              location.pathname === "/"
+                ? "bg-regal-yellow text-white"
+                : "active:bg-regal-yellow hover:text-yellow-300 "
+            } rounded-lg py-2 px-4`}
+          >
             <a href="/">Search</a>
           </li>
 
           <li
             className={`w-20 m-0 h-10 flex items-center justify-center ${
-              location.pathname === '/movies'
-                ? 'bg-regal-yellow text-white'
-                : 'active:bg-regal-yellow hover:text-yellow-300 '
-            } rounded-lg py-2 px-4`}>
+              location.pathname === "/movies"
+                ? "bg-regal-yellow text-white"
+                : "active:bg-regal-yellow hover:text-yellow-300 "
+            } rounded-lg py-2 px-4`}
+          >
             <a href="/movies">Movie</a>
           </li>
 
           <li
             className={`w-20 m-0 h-10 flex items-center justify-center ${
-              location.pathname === '/About'
-                ? 'bg-regal-yellow text-white'
-                : 'active:bg-regal-yellow hover:text-yellow-300'
-            } rounded-lg py-2 px-4`}>
+              location.pathname === "/About"
+                ? "bg-regal-yellow text-white"
+                : "active:bg-regal-yellow hover:text-yellow-300"
+            } rounded-lg py-2 px-4`}
+          >
             <a href="/About">About</a>
           </li>
         </ul>
       </nav>
       <div className="p-2 bg-gray-200 rounded-full ml-auto">
         <a href="/About">
-          <img
-            className="w-8 h-8"
-            src={favicon}
-            alt="heropy"></img>
+          <img className="w-8 h-8" src={favicon} alt="heropy"></img>
         </a>
       </div>
     </header>
-  )
+  );
 }
