d00449b3fc817fdc8827773917383afb63b69f24
pildrums <there7788@gmail.com>
2023-04-26 20:17:59 +0900

    getMovies 함수 매개변수와 url 수정 > 기존의 파라미터에 있던 movieId 삭제 > url에 보간되어 있던 movieId 삭제

--- src/lib/api/movieAPI.js
+++ src/lib/api/movieAPI.js
@@ -2,9 +2,9 @@
   검색된 영화 API 가져오기
   method: 'GET'
 */
-export async function getMovies(title, movieId) {
+export async function getMovies(title) {
   try {
-    const url = `http://omdbapi.com/?apikey=7035c60c&s=${title}&i=${movieId}`;
+    const url = `http://omdbapi.com/?apikey=7035c60c&s=${title}`;
     const response = await fetch(url);
     const result = await response.json();
     return result;
