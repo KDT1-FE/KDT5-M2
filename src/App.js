f830dc63005117b6cffbe0b014d79dd00c0e06f0
pildrums <there7788@gmail.com>
2023-04-26 23:19:04 +0900

    MovieDetailPage 컴포넌트 적용 > 기존의 MoviePage가 없어지고 MovieDetail 적용 > 라우팅의 단순화를 위해서 변경

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import { useCallback, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "src/components/common/Header";
 import ErrorPage from "src/routes/ErrorPage";
 import MainPage from "src/routes/MainPage";
-import MoviePage from "src/routes/MoviePage";
+import MovieDetailPage from "src/routes/MovieDetailPage";
 
 function App() {
-  // Hooks
-  const [category, setCategory] = useState("검색");
-  const onMenuSelect = useCallback((category) => {
-    setCategory(category);
-  }, []);
-
   // Render
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <Header category={category} onMenuSelect={onMenuSelect} />
+      <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/:movieId" element={<MoviePage />} />
+        <Route path="/:movieId" element={<MovieDetailPage />} />
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </Router>
