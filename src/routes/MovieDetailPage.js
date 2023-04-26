9a69241efc43a9be762334bb5b4f881c65f762f9
pildrums <there7788@gmail.com>
2023-04-26 23:24:49 +0900

    MovieDetailPage.js 라우팅 컴포넌트 작성 > 기존의 MoviePage를 없애고 MovieDetailPage를 새로 만듦 > 내부에 있던 Movie 컴포넌트도 SearchDetail로 변경

---
+++ src/routes/MovieDetailPage.js
@@ -0,0 +1,11 @@
+import SearchDetail from "src/components/search/SearchDetail";
+
+function MovieDetailPage() {
+  return (
+    <>
+      <SearchDetail />
+    </>
+  );
+}
+
+export default MovieDetailPage;
