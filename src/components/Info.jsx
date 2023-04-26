626428d287dec498e119c3220bfde7711ca599be
hookor <gerrard1219@gmail.com>
2023-04-27 01:58:41 +0900

    Info_data_filled

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,13 +1,30 @@
-import { React, useEffect } from 'react'
+import { React, useEffect, useState } from 'react'
 import { Stack } from '@mui/material'
 import { Image } from 'mui-image'
 import { useLocation } from 'react-router-dom'
 
 const Info = () => {
   const location = useLocation()
-  console.log(location.state)
   const movieinfo = location.state
-  console.log(location.state.Poster)
+  const movieID = movieinfo.imdbID
+  const [film, setFilm] = useState([])
+
+  async function getMovie(ID) {
+    const res = await fetch(
+      `https://omdbapi.com/?apikey=7035c60c&i=${ID}&plot=full`
+    )
+    const json = await res.json()
+    if (json.Response === 'True') {
+      console.log(json)
+      setFilm(json)
+      console.log(json.Ratings[0].Value)
+    }
+    return json.Error
+  }
+  useEffect(() => {
+    getMovie(movieID)
+  }, [movieID])
+
   return (
     <>
       <Stack
@@ -16,23 +33,37 @@ const Info = () => {
         <Stack
           flexGrow={1}
           ml={6}>
-          <Image src={movieinfo.Poster}></Image>
+          <Image
+            src={movieinfo.Poster}
+            width="400px"
+            height="520px"></Image>
         </Stack>
         <Stack
           flexGrow={1}
           mr={6}
           justifyContent="space-evenly">
-          <Stack flexGrow={3}>Title</Stack>
-          <Stack flexGrow={1}>ReleaseDate,RunningTime,Country</Stack>
-          <Stack flexGrow={8}>Plot</Stack>
-          <Stack flexGrow={1}>Ratings</Stack>
-          <Stack flexGrow={1}>RatingsInfo</Stack>
+          <Stack flexGrow={3}>{film.Title}</Stack>
+          <Stack flexGrow={1}>
+            {film.Released}
+            {film.Runtime}
+            {film.Country}
+          </Stack>
+          <Stack flexGrow={8}>{film.Plot}</Stack>
+          <Stack flexGrow={1}>Rating</Stack>
+          <Stack flexGrow={1}>
+            {film.Ratings[0].Source}
+            {film.Ratings[0].Value}
+            {film.Ratings[1].Source}
+            {film.Ratings[1].Value}
+            {film.Ratings[2].Source}
+            {film.Ratings[2].Value}
+          </Stack>
           <Stack flexGrow={1}>Cast</Stack>
-          <Stack flexGrow={1}>CastList</Stack>
+          <Stack flexGrow={1}>{film.Actors}</Stack>
           <Stack flexGrow={1}>Director</Stack>
-          <Stack flexGrow={1}>DirectorName</Stack>
+          <Stack flexGrow={1}>{film.Director}</Stack>
           <Stack flexGrow={1}>Genre</Stack>
-          <Stack flexGrow={1}>GenreInfo</Stack>
+          <Stack flexGrow={1}>{film.Genre}</Stack>
         </Stack>
       </Stack>
     </>
