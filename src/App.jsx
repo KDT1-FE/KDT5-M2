47a266c45872dc2fa0431e4b71b6ab3c014b4d48
hahahaday12 <hahahaday12@naver.com>
2023-04-26 23:19:13 +0900

    fix : router 부분 추가 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import MainPage from './page/Home/Home'
+import { Routes, Route, BrowserRouter, Outlet } from 'react-router-dom'
+import MainPage from './page/Home/mainPage'
+import Header from './components/common/header'
+import Footer from './components/common/footer'
 import DetailPage from './page/Detail/detailPage'
+import AboutPage from './page/About/aboutPage'
+
 import './scss/App.scss'
 
-function App() {
+const Layout = () =>{
+  return(
+    <div >
+      <Header/>
+        <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
+
+function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
-      <Route path='/' element={<MainPage/>}></Route>
-      <Route path='/movie/:id' element={<DetailPage/>}></Route>
+        <Route path='/' element={<Layout/>}>
+        <Route index element={<MainPage/>}/>
+        <Route path='/about' element={<AboutPage/>}/>
+        <Route path='/movie/:id' element={<DetailPage/>}/>
+          
+        </Route>
       </Routes>
     </BrowserRouter>
     </>
   )
-}
+};
 
 export default App
