a7712b0a1c80c7ae7bbded1d771ec60fd8d0aa6a
pildrums <there7788@gmail.com>
2023-04-28 08:30:52 +0900

    로딩화면에 텍스트 추가

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -6,14 +6,22 @@ function Loading() {
   return (
     <LoadingContainer>
       <BeatLoader size={24} color={colors.gray[0]} />
+      <span>잠시만 기다려주세요</span>
     </LoadingContainer>
   );
 }
 
 const LoadingContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 30px;
+  span {
+    font-size: 20px;
+    font-weight: 700;
+    color: ${colors.gray[0]};
+  }
 `;
 
 export default Loading;
