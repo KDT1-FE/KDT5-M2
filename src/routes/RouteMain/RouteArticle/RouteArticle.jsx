d010a1992030a30d7a180dafc47c53b1c7b21530
Kled <1017yu@daum.net>
2023-04-27 19:56:24 +0900

    docs: Ratings 기능 추가로 인한 수정

---
+++ src/routes/RouteMain/RouteArticle/RouteArticle.jsx
@@ -0,0 +1,48 @@
+import styles from "./RouteArticle.module.scss";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { axiosDetailMovies } from "../../../core/movieDetailData";
+import { NavLink } from "react-router-dom";
+
+const RouteArticle = () => {
+  const [movie, setMovie] = useState({});
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    async function detailMovies(e) {
+      const detail = await axiosDetailMovies(movieId);
+      setMovie(detail);
+    }
+    detailMovies(movieId);
+  }, [movieId]);
+
+  return (
+    <>
+      <article className={styles.mainArticle}>
+        <div className={styles.detailInfo}>
+          <ul className={styles.tapMenu}>
+            <li>
+              <NavLink
+                to={`/movie/main/${movieId}`}
+                className={styles.routeNavLink}
+              >
+                <span>주요정보</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/movie/ratings/${movieId}`}
+                className={styles.routeNavLink}
+              >
+                <span>평점</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.articleWrapper}>{movie.Plot}</div>
+      </article>
+    </>
+  );
+};
+
+export default RouteArticle;
---
+++ src/routes/RouteMain/RouteSection/RouteSection.jsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { axiosDetailMovies } from "../../../core/movieDetailData";
+import styles from "./RouteSection.module.scss";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import ZoomOutMapIcon from "@mui/icons-material/ZoomOutMap";
+
+const RouteSection = () => {
+  const [movie, setMovie] = useState({});
+  // 모달창 노출 여부 false
+  const [open, setOpen] = useState(false);
+
+  // 모달창 제어
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const { movieId } = useParams();
+
+  // mui 라이브러리 ZoomOut icon styling
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "25%",
+    transform: "translate(-50%, -50%)",
+    width: 500,
+    height: 740,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+    borderRadius: 5,
+  };
+  useEffect(() => {
+    async function detailMovies(e) {
+      const detail = await axiosDetailMovies(movieId);
+      setMovie(detail);
+    }
+    detailMovies(movieId);
+  }, [movieId]);
+
+  const bigPoster = movie.Poster ? movie.Poster.replace("SX300", "SX500") : "";
+
+  return (
+    <section className={styles.mainSection}>
+      <div className={styles.movieContainer}>
+        <div
+          className={styles.moviePoster}
+          style={{ background: `url(${movie.Poster})` }}
+        >
+          <Button onClick={handleOpen} className={styles.zoomOutButton}>
+            <ZoomOutMapIcon className={styles.zoomOutMapIcon} />
+          </Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style} style={{ background: `url(${bigPoster})` }}></Box>
+          </Modal>
+        </div>
+
+        <div className={styles.movieInfo}>
+          <h1 className={styles.movieTitle}>{movie.Title}</h1>
+          <div className={styles.movieDetail}>
+            <div className={styles.movieDetailInner}>
+              <div className={styles.movieDetailList}>
+                <div className={styles.movieDetailGray}>개봉</div>
+                <div className={styles.movieDetailWhite}>{movie.Released}</div>
+              </div>
+              <div className={styles.movieDetailList}>
+                <div className={styles.movieDetailGray}>장르</div>
+                <div className={styles.movieDetailWhite}>{movie.Genre}</div>
+              </div>
+              <div className={styles.movieDetailList}>
+                <div className={styles.movieDetailGray}>국가</div>
+                <div className={styles.movieDetailWhite}>{movie.Country}</div>
+              </div>
+              <div className={styles.movieDetailList}>
+                <div className={styles.movieDetailGray}>러닝타임</div>
+                <div className={styles.movieDetailWhite}>{movie.Runtime}</div>
+              </div>
+            </div>
+            <div className={styles.movieDetailInner}>
+              <div className={styles.movieDetailList}>
+                <div className={styles.movieDetailGray}>평점</div>
+                <div className={styles.movieDetailWhite}>
+                  {movie.imdbRating}
+                </div>
+              </div>
+              <div className={styles.movieDetailList}>
+                <div className={styles.movieDetailGray}>누적수입</div>
+                <div className={styles.movieDetailWhite}>{movie.BoxOffice}</div>
+              </div>
+              <div className={styles.movieDetailList}>
+                <div className={styles.movieDetailGray}>배우</div>
+                <div className={styles.movieDetailWhite}>{movie.Actors}</div>
+              </div>
+              <div className={styles.movieDetailList}>
+                <div className={styles.movieDetailGray}>감독</div>
+                <div className={styles.movieDetailWhite}>{movie.Director}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RouteSection;
