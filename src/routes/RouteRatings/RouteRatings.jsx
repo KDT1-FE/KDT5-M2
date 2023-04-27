eb76a6c782d162631cbcddef1151a050fe97eff8
Kled <1017yu@daum.net>
2023-04-27 19:56:05 +0900

    feat: Ratings 기능 추가

---
+++ src/routes/RouteRatings/RouteRatings.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RouteSection from "../RouteMain/RouteSection/RouteSection";
+import RouteRatingsArticle from "./RouteRatingsArticle";
+
+const RouteRatings = () => {
+  return (
+    <main>
+      <RouteSection />
+      <RouteRatingsArticle />
+    </main>
+  );
+};
+export default RouteRatings;
