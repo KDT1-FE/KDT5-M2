ba86e44acf24e1ab87c9fdc73b8f30f61f0f34e6
KIMGYEONGWON <ruddnjs6651@gmail.com>
2023-04-29 02:43:40 +0900

    aboutpage, footer 추가

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,19 @@ function About($container) {
   this.render = () => {
     this.$container.innerHTML = `
       <main class="about">
-        어바웃 페이지에요.
+      <img class="smile" src="/movietour_logo_smile.svg" alt="스마일">
+        <h2>
+        OMDB <span>API</span>
+        <br/>
+        <p> 영화검색 사이트 <span>만들기</span> </p>
+        </h2>
+
+        <h3>패스트캠퍼스 FE5_김경원</h3>
+        <a href="https://github.com/ruddnjs3769">
+        <img src="/github-mark.png" alt="github"/>
+        github
+        </a>
+
       </main>
     `;
   };
