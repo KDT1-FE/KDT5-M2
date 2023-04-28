00a30c79060c9df96327b25b17d39bb3502acfd0
Kled <1017yu@daum.net>
2023-04-28 17:40:57 +0900

    docs: Module 연결

--- src/components/Main/SearchInput/Select.jsx
+++ src/components/Main/SearchInput/Select.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import styles from "./Select.module.scss";
 
 const Select = (props) => {
   return (
-    <select name={props.category} onChange={props.onChange}>
+    <select
+      name={props.category}
+      onChange={props.onChange}
+      className={styles.select}
+    >
       {props.options.map((option) => (
         <option key={option} value={option}>
           {option}
