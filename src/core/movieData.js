fee344a2175f0ad094821e64b606ed624cf91504
Kled <1017yu@daum.net>
2023-04-28 16:27:56 +0900

    docs: 카테고리 검색을 위한 axios 호출 변경

--- src/core/movieData.js
+++ src/core/movieData.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-export async function axiosMovies(inputText, page) {
+export async function axiosMovies(inputText, year, type, page) {
   const { data } = await axios({
-    url: `https://omdbapi.com/?apikey=7035c60c&s=${inputText}&page=${page}`,
+    url: `https://omdbapi.com/?apikey=7035c60c&s=${inputText}&y=${year}&type=${type}&page=${page}`,
     method: "GET",
   });
-
   return data;
 }
