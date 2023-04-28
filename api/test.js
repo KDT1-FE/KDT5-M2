5911286f9088bc0c9f032e857b2140364842f80d
Tteum00 <wjdtmddnjs0913@naver.com>
2023-04-28 21:00:31 +0900

    오류 수정

--- api/test.js
+++
@@ -1,7 +0,0 @@
-export default function handler(requst, response) {
-  response.status(200).json({
-    name: "Tteum",
-    age: 20,
-    isValid: true
-  })
-}
--- src/store/movie.js
+++ src/store/movie.js
@@ -21,7 +21,13 @@ export const searchMovies = async page => {
     store.state.message = ''
   }
   try {
-    const res = await fetch(`https://omdbapi.com?apikey=${APIKEY}&s=${store.state.searchText}&page=${page}`)
+  const res = await fetch('/api/movie', {
+    method: 'POST',
+    body: JSON.stringify ({
+      title: store.state.searchText,  
+      page
+    })
+  })
   const { Search, totalResults, Response, Error } = await res.json()
   if (Response === 'True') {
     store.state.movies = [
@@ -40,7 +46,12 @@ export const searchMovies = async page => {
 }
 export const getMovieDetails = async id => {
   try {
-    const res = await fetch(`https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`)
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        id
+      })
+    })
     store.state.movie = await res.json()
   } catch (error) {
     console.log('getMovieDetails error:', error)
