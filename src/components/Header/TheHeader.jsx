f1b988f28a3d175f58641a239092940c3c7a2ae5
Kled <1017yu@daum.net>
2023-04-30 17:57:17 +0900

    feat: Header Home 추가

--- src/components/Header/TheHeader.jsx
+++ src/components/Header/TheHeader.jsx
@@ -5,12 +5,26 @@ import styles from "./TheHeader.module.scss";
 const TheHeader = () => {
   return (
     <header>
-      <NavLink to="/" className={styles.logo}>
-        WUTCHA
-      </NavLink>
-      <NavLink to="/movie/main/tt2194499" className={styles.movie}>
-        영화
-      </NavLink>
+      <nav>
+        <NavLink to="/" className={styles.navWutcha}>
+          WUTCHA
+        </NavLink>
+      </nav>
+      <nav>
+        <a href="/" className={styles.navHome}>
+          홈
+        </a>
+      </nav>
+      <nav>
+        <NavLink to="/movie/main/tt2194499" className={styles.navMovie}>
+          영화
+        </NavLink>
+      </nav>
+      <nav>
+        <NavLink to="/about" className={styles.navAbout}>
+          더보기
+        </NavLink>
+      </nav>
     </header>
   );
 };
