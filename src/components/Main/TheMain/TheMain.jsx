2a0dfb65225ce4990b390739df7a9723662e6f6b
Kled <1017yu@daum.net>
2023-04-30 17:58:53 +0900

    docs: TheMain 모듈 연결

--- src/components/Main/TheMain/TheMain.jsx
+++ src/components/Main/TheMain/TheMain.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Article from "../Article/Article";
 import SearchInput from "../SearchInput/SearchInput";
+import styles from "./TheMain.module.scss";
 
 const TheMain = () => {
   return (
-    <main>
+    <main className={styles.theMain}>
       <Article />
       <SearchInput />
     </main>
