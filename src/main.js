fba88463dfabfef3b8ac90b9ea5cabc2eb658f9c
BearHumanS <bye0651@naver.com>
2023-05-02 20:50:18 +0900

    update

--- src/main.js
+++ src/main.js
@@ -37,8 +37,7 @@ async function fetchMovies(searchInputVal, page, year) {
   )
   const data = await response.json()
   return data.Search
-  }
-
+}
 searchInput.addEventListener('keydown', async e => {
   if (e.key === 'Enter') {
     removeSearching()
@@ -55,7 +54,7 @@ searchInput.addEventListener('keydown', async e => {
         return `
           <div class="swiper-slide">
             <h2>${Title} (${Year})</h2>
-            <p><img class="poster" src=${Poster} alt="${Title}" onerror=" this.src='./static/No-image-available.jpg';" onclick="getDetails('${imdbID}')">
+            <p><img class="poster" src=${Poster} alt="${Title}" onerror="this.onerror=null; this.src='/No-image-available.jpg';" onclick="getDetails('${imdbID}')">
             </p>
             <div class="movie-data" style="display: none;"></div>
           </div>
