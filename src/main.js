46d5146f00167f79bfaef9ecc5d5659708f39daf
Kled <1017yu@daum.net>
2023-05-04 00:24:33 +0900

    docs: router 수정

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "~/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { RouterProvider } from "react-router-dom";
-import router from "~/routes";
+import router from "~/core/BrowserRouter";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
