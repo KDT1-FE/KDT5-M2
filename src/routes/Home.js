a2c4a46ec518df8efce18ec39dae07b3c44bf25d
azure0929 <chary8028@naver.com>
2023-05-03 09:48:27 +0900

    Section-movie-related setting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -28,6 +28,90 @@ export default class Home extends Component {
             </ul>
           </div>
         </section>
+        <!-- Section : Movie-related -->
+        <section class="movie-related">
+          <h3>RELATED MOVIES</h3>
+          <ul class="related-movies">
+            <li>
+              <div class="related-poster">
+                <a 
+                  href="https://www.marvel.com/movies/spider-man-far-from-home" 
+                  target="_blank">
+                  <img src="https://cdn.marvel.com/content/1x/spider-manfarfromhome_lob_crd_04_3.jpg" />
+                </a>
+              </div>
+              <div class="related-info">
+                <h4>Spider-Man: Far-From Home</h4>
+                <span>2019</span>
+              </div>
+            </li>
+            <li>
+              <div class="related-poster">
+                <a 
+                  href="https://www.marvel.com/movies/avengers-endgame" 
+                  target="_blank">
+                  <img src="https://cdn.marvel.com/content/1x/avengersendgame_lob_crd_05_2.jpg" />
+                </a>
+              </div>
+              <div class="related-info">
+                <h4>Avengers: Endgame</h4>
+                <span>2019</span>
+              </div>
+            </li>
+            <li>
+              <div class="related-poster">
+                <a 
+                  href="https://www.marvel.com/movies/captain-marvel" 
+                  target="_blank">
+                  <img src="https://cdn.marvel.com/content/1x/captainmarvel_lob_crd_06.jpg" />
+                </a>
+              </div>
+              <div class="related-info">
+                <h4>Captain Marvel</h4>
+                <span>2019</span>
+              </div>
+            </li>
+            <li>
+              <div class="related-poster">
+                <a 
+                  href="https://www.marvel.com/movies/ant-man-and-the-wasp" 
+                  target="_blank">
+                  <img src="https://cdn.marvel.com/content/1x/ant-manthewasp_lob_crd_01.jpg" />
+                </a>
+              </div>
+              <div class="related-info">
+                <h4>Ant-Man and The Wasp</h4>
+                <span>2018</span>
+              </div>
+            </li>
+            <li>
+              <div class="related-poster">
+                <a 
+                  href="https://www.marvel.com/movies/black-panther" 
+                  target="_blank">
+                  <img src="https://cdn.marvel.com/content/1x/blackpanther_lob_crd_01_4.jpg" />
+                </a>
+              </div>
+              <div class="related-info">
+                <h4>Black Panther</h4>
+                <span>2018</span>
+              </div>
+            </li>
+            <li>
+              <div class="related-poster">
+                <a 
+                  href="https://www.marvel.com/movies/thor-ragnarok" 
+                  target="_blank">
+                  <img src="https://cdn.marvel.com/content/1x/thorragnarok_lob_crd_03.jpg" />
+                </a>
+              </div>
+              <div class="related-info">
+                <h4>Thor: Ragnarok</h4>
+                <span>2017</span>
+              </div>
+            </li>
+          </ul>
+        </section>
       </div>
     `
   }
