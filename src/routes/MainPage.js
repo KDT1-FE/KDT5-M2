6bb35d6eedb99a82cac49a670f2c9571b7907488
pildrums <there7788@gmail.com>
2023-05-03 19:44:00 +0900

    props로 넘겨줬던 state 이동 > value, setValue state를 search/Search.js로 이동

--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -1,22 +1,13 @@
-import { useState } from "react";
 import Search from "src/components/search/Search";
 import SearchMain from "src/components/search/SearchMain";
 
 // Component
 function MainPage() {
-  // Hooks
-  const [value, setValue] = useState("");
-  const [movies, setMovies] = useState([]);
   // Render
   return (
     <>
-      <SearchMain movies={movies} setMovies={setMovies} />
-      <Search
-        movies={movies}
-        value={value}
-        setMovies={setMovies}
-        setValue={setValue}
-      />
+      <SearchMain />
+      <Search />
     </>
   );
 }
