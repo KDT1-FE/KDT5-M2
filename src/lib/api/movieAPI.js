29e74b8a9878b887f47d7d4982da1a69613c7940
pildrums <there7788@gmail.com>
2023-05-03 19:46:48 +0900

    무한 스크롤 구현을 위한 API 호출 함수 수정 > 단순하게 API를 요청했던 로직을 for 문을 이용해서 page가 자동으로 하나씩 더해지게끔 로직 변경

--- src/lib/api/movieAPI.js
+++ src/lib/api/movieAPI.js
@@ -1,23 +1,18 @@
-import axios from "axios";
 /*
   검색된 영화 API 가져오기
   method: 'GET'
 */
-export async function getMovies(title, page, year) {
+export async function getMovies(title, type, years, num) {
+  let movies = [];
   try {
-    const { data } = await axios({
-      url: `http://omdbapi.com/?apikey=7035c60c&s=${title}&y=${year}&page=${page}`,
-      method: "GET",
-    });
-    return data;
+    for (let page = 1; page <= num; page++) {
+      const url = `https://omdbapi.com/?apikey=7035c60c&s=${title}&type=${type}&page=${page}&y=${years}`;
+      const response = await fetch(url);
+      const result = await response.json();
+      movies = [...movies, ...result.Search];
+    }
+    return movies;
   } catch (e) {
     console.error(e, "API 요청에 실패했습니다.");
   }
-  // const result = await axios
-  //   .get(`http://omdbapi.com/?apikey=7035c60c&s=${title}&page=${page}`)
-  //   .then((res) => res.data)
-  //   .catch();
-  // return {
-  //   Search: [...result.Search],
-  // };
 }
