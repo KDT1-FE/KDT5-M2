2037d8ba4598701d61de48fd2e7378f8a47d63cc
ahntaeuk <dotory0829@naver.com>
2023-05-07 15:08:46 +0900

    omdbMovi-project

---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+import { Store } from '../core/mainComponent'
+
+export default new Store({
+  photo: '/Users/ahntaeuk/Desktop/movie-app/profile.jpg',
+  name: 'Ahntaeuk',
+  email: 'dotory0829@gmail.com',
+  github: 'https://github.com/dotory0829',
+  repository: 'x'
+})
