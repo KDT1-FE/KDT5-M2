6b28f57d8adb11cc60b655060f8700a641eddd4b
howooking <junsgk@gmail.com>
2023-05-08 10:50:39 +0900

    리뷰2. type 수정

--- src/constants/selectItems.ts
+++ src/constants/selectItems.ts
@@ -1,8 +1,8 @@
 export const shows = ['movie', 'series', 'episodes'];
 export const page = ['10', '20', '30'];
 
-const years: number[] = [];
+const years: string[] = [];
 for (let i = 2023; i >= 1985; i--) {
-  years.push(i);
+  years.push(i.toString());
 }
 export const allYears = ['All Years', ...years];
