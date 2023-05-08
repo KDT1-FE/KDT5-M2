1df2a5f714ff1475753a13cfdd5a311ec40799c7
howooking <junsgk@gmail.com>
2023-05-08 10:44:54 +0900

    리뷰2. constants/selectItmes.ts
    - new Date().getFullYear는 함수 실행 결과이므로 상수가 아님.
    - 애초에 for 반복문 과정에서 순서를 정해줄 수 있으므로 sort가 필요없음.

--- src/constants/selectItems.ts
+++ src/constants/selectItems.ts
@@ -2,8 +2,7 @@ export const shows = ['movie', 'series', 'episodes'];
 export const page = ['10', '20', '30'];
 
 const years: number[] = [];
-for (let i = 1985; i <= new Date().getFullYear(); i++) {
+for (let i = 2023; i >= 1985; i--) {
   years.push(i);
 }
-const sortedYears = years.sort((a, b) => b - a).map((year) => year.toString());
-export const allYears = ['All Years', ...sortedYears];
+export const allYears = ['All Years', ...years];
