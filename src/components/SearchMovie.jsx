4bcbf3cd9c8826fa17d60b37a95d64c8e53d216a
EUNJI <eunji9180@naver.com>
2023-05-09 19:57:42 +0900

    230509 2차과제 리팩토링 -2

--- src/components/SearchMovie.jsx
+++ src/components/SearchMovie.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styles from '../styles/SearchMovie.module.scss'
 
@@ -6,6 +6,23 @@ export default function SearchMovie() {
   const [searchQuery, setSearchQuery] = useState('') //검색어
   const [movieList, setMovieList] = useState([]) //영화 목록
   const navigate = useNavigate() //페이지 이동
+
+  //컴포넌트가 Mount되었을 때 실행, 브라우저 세션스토리지에 저장된 검색어, 결과를 가져와서 초기화
+  useEffect(() => {
+    //sessionStorage의 데이터는 페이지 세션이 끝날 때 제거됨
+    const savedQuery = sessionStorage.getItem('searchQuery')
+    const savedResults = sessionStorage.getItem('movieList')
+    if (savedQuery && savedResults) {
+      setSearchQuery(savedQuery)
+      setMovieList(JSON.parse(savedResults))
+    }
+  }, [])
+
+  //검색어와 검색결과가 바뀔 때마다 실행
+  useEffect(() => {
+    sessionStorage.setItem('searchQuery', searchQuery)
+    sessionStorage.setItem('movieList', JSON.stringify(movieList))
+  }, [searchQuery, movieList])
 
   // 영화검색 API요청 보내기
   async function fetchMovieList(title, year = '', page = 1) {
@@ -76,9 +93,12 @@ export default function SearchMovie() {
           />
           <button type="submit">Search</button>
         </form>
-        <ul id={styles.movieList}>
-          {movieList.length > 0 &&
-            movieList.map(movie => (
+        {/* movieList가 undefined일 경우를 조건 추가*/}
+        {movieList?.length === 0 && console.log('No Found Movie')}
+
+        {movieList?.length > 0 && (
+          <ul id={styles.movieList}>
+            {movieList.map(movie => (
               <div
                 className={styles.movieItem}
                 key={movie.imdbID}
@@ -95,7 +115,8 @@ export default function SearchMovie() {
                 <h2 className={styles.movieTitle}>{movie.Title}</h2>
               </div>
             ))}
-        </ul>
+          </ul>
+        )}
         {movieList?.length > 0 && (
           <button
             className={styles.viewMore}
