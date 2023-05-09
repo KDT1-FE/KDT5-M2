033daed3b734e1d0f9087ef58f1ec32cf173bdb6
unknown <kkydu009@gmail.com>
2023-05-09 15:47:34 +0900

    과제제출

---
+++ src/store/movie.js
@@ -0,0 +1,34 @@
+import { Store } from '../core/heropy'
+
+const store = new Store({
+  searchText: '',
+  page: 1,
+  pageMax: 1,
+  movies: [],
+})
+
+export default store
+export const searchMovies = async page => {
+  if (page === 1) {
+  store.state.loading = true
+  store.state.page = page
+  store.state.movies = []
+  }
+  try {
+  const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`)
+    const { Search, totalResults, Response, Error } = await res.json()
+    if (Response === 'True') {
+      store.state.movies = [
+        ...store.state.movies,
+        ...Search
+    ]
+    store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+    } else {
+      store.state.message = Error
+    }
+  } catch (error) {
+    console.log('searchMovies error:', error)
+  } finally {
+    store.state.loading = false
+  }
+}
